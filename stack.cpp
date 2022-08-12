ac4455c860bdc71747dde5601da44b4cfdb68d6e
ojesAtul <atuljnv02@gmail.com>
2022-08-13 02:24:09 +0530

    added  two stack

--- stack.cpp
+++ stack.cpp
@@ -22,5 +22,47 @@ public:
          exit(0);
         vec.pop_back(); 
     }
-
+};
+// implement two stack into the same array
+class toStack{
+private:
+    int arr[10000];
+    int left=-1,right=10000;
+public:    
+    void push1(int val){
+        if(left+1==right){
+            exit(0);
+        }
+        arr[++left]=val;
+    }
+    void push2(int val){
+        if(right-1==left)
+         exit(0);
+        arr[--right]=val; 
+    }
+    void pop1(){
+        if(left==-1){
+            exit(0);
+        }
+        left--;
+    }
+    void pop2(){
+        if(right==10000){
+            exit(0);
+        }
+        right++;
+    }
+    int top1(){
+        if(left==-1)
+          exit(0);
+        return arr[left];  
+    }
+    int top2(){
+        if(right==10000)
+           exit(0);
+        return arr[right];   
+    }
+   int sizeOf(){
+    return left+1+10000-right;
+   }
 };
