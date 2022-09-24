da3ce8f77a6b9a265a038c277ac0e0bcf2a41e5f
Atul Yadav <atuljnv02@gmail.com>
2022-09-24 18:41:37 +0530

    updated the trie code

--- trie.cpp
+++ trie.cpp
@@ -1,13 +1,5 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-
-// think also about it  iterative implemematation
-
-/*
-    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
-*/
-
 struct node{
     int cnt;
     bool end;
@@ -62,32 +54,31 @@ bool delete_string(string &str,int pos,node* root){
         root->child[str[pos]-'a']=NULL;
         delete(temp);
     }
-    void mirror_image(node* root){
-        if(!root){
-            return;
-        }
-        node* temp=root->left;
-        root->left=root->right;
-        root->right=temp;
-        mirror_image(root->left);
-        mirror_image(root->right);
-    }
-    bool isSymmetric(node* tree1,node* tree2){
-       if(!tree1 && !tree2)
-        return true;
-        else if(!tree1){
-        return false;
-        }  
-        else if(!tree2)
-        return false;
-        else if(tree1->val!=tree2->val)
-         return false;
-        return isSymeetric(tree1->left,tree2->right)&&isSymmetric(tree1->right,tree2->left); 
-    }
-    bool symmetricBinarytree(node* root){
-        if(!root)
-          return true;
-        return isSymetric(root->left,root->right);  
-    }
-
+}
+void mirror_image(node* root){
+	if(!root){
+		return;
+	}
+	node* temp=root->left;
+	root->left=root->right;
+	root->right=temp;
+	mirror_image(root->left);
+	mirror_image(root->right);
+}
+bool isSymmetric(node* tree1,node* tree2){
+	if(!tree1 && !tree2)
+	return true;
+	else if(!tree1){
+	return false;
+	}  
+	else if(!tree2)
+	return false;
+	else if(tree1->val!=tree2->val)
+		return false;
+	return isSymeetric(tree1->left,tree2->right)&&isSymmetric(tree1->right,tree2->left); 
+}
+bool symmetricBinarytree(node* root){
+	if(!root)
+		return true;
+	return isSymetric(root->left,root->right);  
 }
